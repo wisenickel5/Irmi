10848847f26ba75a14274c836e0e9d413a0fc0a6
Dylan Alexander <dylanjalexander2@gmail.com>
2023-02-22 21:25:41 -0500

    Created fancy hello world

--- Irmi/Program.cs
+++ Irmi/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// A little bit more fancy version of Hello World...
+Console.WriteLine("What is your name?");
 
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
