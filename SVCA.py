b3c5c8f4bf804201f95106f58770a765316a6abd
Dylan Alexander <dylanjalexander2@gmail.com>
2023-04-16 21:07:28 -0400

    Formatting changes to SVCA code
    Made changes to make a bit more readable and add inline documentation

--- SVCA.py
+++ SVCA.py
@@ -6,21 +6,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 
-# simple construct object to hold a modles weights or other stuff if we want
-
 class IVP:
-
+    """
+    Simple construct object to hold a model's weights or other stuff if we want
+    """
     def __init__(self, model):
         self.model = model
 
 
-# this computes the linear transformtion of our vector into classifcation space R^n
 def calc_Wx(W, x):
+    """
+    Computes the linear transformation of our vector into classification space R^n
+    :param W:
+    :param x:
+    :return:
+    """
     return W.dot(x)
 
 
-# data preprocessing method one
 def norm_column(column):
+    """
+    Data preprocessing method one
+    :param column:
+    :return:
+    """
     w = max(column)
     for i in range(0, len(column)):
         column[i] = (column[i]) / (w + .0001)
@@ -33,8 +42,12 @@ def norm_data(data):
     return data
 
 
-# This is the measurement function
 def prediction(P):
+    """
+    Measurement function
+    :param P:
+    :return:
+    """
     maxi = 0
     for i in range(0, len(P)):
         if P[i] > P[maxi]:
@@ -42,9 +55,16 @@ def prediction(P):
     return maxi
 
 
-# This update function is essentially a training function, it finds the optimal transformation W
-# update predictions
 def update(W, x, y, a, c):
+    """
+    This update function is essentially a training function, it finds the optimal transformation W update predictions
+    :param W:
+    :param x:
+    :param y:
+    :param a:
+    :param c:
+    :return:
+    """
     P = prediction(calc_Wx(W, x))
     if P != y:
         for i in range(0, len(W)):
@@ -55,8 +75,13 @@ def update(W, x, y, a, c):
     return W
 
 
-# algorithm body
 def IVP(data, tz):
+    """
+    Algorithm body
+    :param data:
+    :param tz:
+    :return:
+    """
     X = data.data
     y = data.target
     le = LabelEncoder()
@@ -80,31 +105,36 @@ def IVP(data, tz):
 
     for i in range(0, len(X_test)):
         p = prediction(calc_Wx(W, X_test[i]))
-        if (y_test[i] == p):
+        if y_test[i] == p:
             c += 1
     return c / len(X_test)
 
 
-# Driver Code.
-numbers = datasets.load_digits()
-cancer = datasets.load_breast_cancer()
-wine = datasets.load_wine()
-iris = datasets.load_iris()
-c1 = 0
-c2 = 0
-c3 = 0
-c4 = 0
-fc = 0
-for i in range(0, 5):
-    I = IVP(iris, .5)
-    w = IVP(wine, .5)
-    c = IVP(cancer, .5)
-    N = IVP(numbers, .8)
-    if (I or w or c) < .7:
-        fc += 1
-    c1 += I
-    c2 += w
-    c3 += c
-    c4 += N
-print('Avg Iris:', c1 / 5, ' Avg Wine:', c2 / 5, ' Avg Cancer:', c3 / 5, ' Avg Numbers:', c4 / 5, 'fail count ',
-      fc / 100)
+if __name__ == "__main__":
+    # Driver Code.
+    numbers = datasets.load_digits()
+    cancer = datasets.load_breast_cancer()
+    wine = datasets.load_wine()
+    iris = datasets.load_iris()
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    fc = 0
+    for i in range(0, 5):
+        I = IVP(iris, .5)
+        w = IVP(wine, .5)
+        c = IVP(cancer, .5)
+        N = IVP(numbers, .8)
+        if (I or w or c) < .7:
+            fc += 1
+        c1 += I
+        c2 += w
+        c3 += c
+        c4 += N
+
+    print('Avg Iris:', c1 / 5,
+          ' Avg Wine:', c2 / 5,
+          ' Avg Cancer:', c3 / 5,
+          ' Avg Numbers:', c4 / 5,
+          'fail count ', fc / 100)
