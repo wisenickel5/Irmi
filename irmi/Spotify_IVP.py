63e096e5cbc13ca5b764de3261b76e7a1bad02b3
Ethan Hunt <107641806+EthanHunt2125@users.noreply.github.com>
2023-04-17 21:22:29 -0400

    Update Spotify_IVP.py
    just created SVCA compatibility

--- irmi/Spotify_IVP.py
+++ irmi/Spotify_IVP.py
@@ -1,20 +1,56 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import random
-import pandas as pd
 import numpy as np
-from sklearn import datasets
-from sklearn import preprocessing
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+import spotipy.util as util
+import SVCA
+import pandas as pd
 
-client_credentials_manager = SpotifyClientCredentials(client_id="12ba2ba13acd401a84603dfb6f678414",
-                                                      client_secret="3dd66fc8821b44a58ad1f43081857ab4")
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def append_audio_features(df, spotify_auth, return_feat_df=False):
+    #this is imported toutrial 
+    audio_features = spotify_auth.audio_features(df["track_id"][:])
+    # catch and delete songs that have no audio features
+    if None in audio_features:
+        NA_idx = [i for i, v in enumerate(audio_features) if v == None]
+        df.drop(NA_idx, inplace=True)
+        for i in NA_idx:
+            audio_features.pop(i)
+    assert len(audio_features) == len(df["track_id"][:])
+    feature_cols = list(audio_features[0].keys())[:-7]
+    features_list = []
+    for features in audio_features:
+        try:
+            song_features = [features[col] for col in feature_cols]
+            features_list.append(song_features)
+        except TypeError:
+            pass
+    df_features = pd.DataFrame(features_list, columns=feature_cols)
+    df = pd.concat([df, df_features], axis=1)
+    if return_feat_df == False:
+        return df
+    else:
+        return df, df_features
+
+def authenticate(redirect_uri, client_cred_manager, username, scope, client_id, client_secret):
+    #this is imported toutrial 
+    sp = spotipy.Spotify(client_credentials_manager=client_cred_manager)
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print("No token for", username)
+    return sp
 
 
-# Authentication - without user
+client_id = "2cbda720019447bb8841a09360cd422d"
+client_secret = "51f6f2ae04dd4b57ae64f961d1556a5d"
+username = "EthanHunt2125"
+redirect_uri = "https://developer.spotify.com/dashboard/applications/2cbda720019447bb8841a09360cd422d"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
+                                                      client_secret=client_secret)
+scope = "user-library-read user-read-recently-played user-top-read playlist-modify-public playlist-read-private playlist-read-collaborative"
+sp = authenticate(redirect_uri, client_credentials_manager, username, scope, client_id, client_secret)
+
+
 def get_taste_vector(taste_data):
     taste_vector = list()
     for i in range(0, taste_data.shape[1]):
@@ -25,15 +61,23 @@ def get_taste_vector(taste_data):
 def create_set_of_good_songs(taste_data):
     taste_vector = list()
     for i in range(0, len(taste_data)):
-        taste_vector.append((taste_data[i], 0))
+        taste_vector.append([taste_data[i], 0])
     return taste_vector
 
 
 def create_set_of_bad_songs(taste_data):
     taste_vector = list()
     for i in range(0, len(taste_data)):
-        taste_vector.append((taste_data[i], 1))
+        taste_vector.append([taste_data[i], 1])
     return taste_vector
+
+
+def create_set_of_potential(data, W):
+    vector = list()
+    for i in range(0, len(data)):
+        if SVCA.prediction(SVCA.calc_Wx(W, np.array(data[i]))) == 0:
+            vector.append(data[i])
+    return vector
 
 
 def get_playlist_data(playlist_URI, Nlist):
@@ -66,6 +110,21 @@ def get_playlist_data(playlist_URI, Nlist):
     return Nlist
 
 
+def get_user_data(data):
+    Mlist = list()
+    Nlist = list()
+    for i in range(0, len(data)):
+        # URI
+        track_uri = data[i]["track"]["uri"]
+        Mlist.append([sp.audio_features(track_uri)[0], track_uri])
+
+    for i in range(0, len(Mlist)):
+        Nlist.append(list(Mlist[i][0].values())[:9])
+    return Nlist
+
+
+results = sp.current_user_saved_tracks()["items"]
+results = get_user_data(results)
 good_playlist_link = "https://open.spotify.com/playlist/4FJ7NmgSV8ttdozvfc6EgN?si=9dbbe50c19d44b56"
 bad_playlist_link = "https://open.spotify.com/playlist/2OdWS5qgdX4oQEQQrqHYZe?si=afe1076fd7d14494"
 playlist_URI = good_playlist_link.split("/")[-1].split("?")[0]
@@ -79,4 +138,11 @@ Nlist2 = get_playlist_data(playlist_URI, Nlist2)
 GS = create_set_of_good_songs(Nlist)
 BS = create_set_of_bad_songs(Nlist2)
 data = GS + BS
+Score, W = SVCA.IVP(data, .1)
+MS=create_set_of_potential(results,W)
+print(MS)
+Mood_matrix = SVCA.MODEL(W)
+
+print(Mood_matrix.model)
+
 print(data)
