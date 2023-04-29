bd69568c6743fe6e637ea00a12c6c6df5edfea72
ethan <ethanhunt2125@gmail.com>
2023-04-28 21:54:37 -0400

    commit Example of how to use new functions.
    results = sp.current_user_saved_tracks()["items"]
    results,URIS = get_user_data(results)
    good_playlist_link = "https://open.spotify.com/playlist/4FJ7NmgSV8ttdozvfc6EgN?si=9dbbe50c19d44b56"
    bad_playlist_link = "https://open.spotify.com/playlist/2OdWS5qgdX4oQEQQrqHYZe?si=afe1076fd7d14494"
    playlist_URI = good_playlist_link.split("/")[-1].split("?")[0]
    playlist_URI2 = bad_playlist_link.split("/")[-1].split("?")[0]
    track_uris = [x["track"]["uri"] for x in sp.playlist_items(playlist_URI)["items"]]
    
    Nlist = list()
    Nlist2 = list()
    Nlist = get_playlist_data(playlist_URI, Nlist)
    Nlist2 = get_playlist_data(playlist_URI, Nlist2)
    GS = create_set_of_good_songs(Nlist)
    BS = create_set_of_bad_songs(Nlist2)
    data = GS + BS
    Score, W = SVCA.IVP(data, .1)
    MS,MURIS=create_set_of_potential(results,URIS,W)

--- irmi/services.py
+++ irmi/services.py
@@ -1,6 +1,86 @@
+import numpy as np
 from flask import redirect
 
+from irmi import SVCA
 from irmi.authenticate import make_get_request
+
+
+def get_taste_vector(taste_data):
+    taste_vector = list()
+    for i in range(0, taste_data.shape[1]):
+        taste_vector.append(np.mean(taste_data[:, i]))
+    return taste_vector
+
+
+def create_set_of_good_songs(taste_data):
+    taste_vector = list()
+    for i in range(0, len(taste_data)):
+        taste_vector.append([taste_data[i], 0])
+    return taste_vector
+
+
+def create_set_of_bad_songs(taste_data):
+    taste_vector = list()
+    for i in range(0, len(taste_data)):
+        taste_vector.append([taste_data[i], 1])
+    return taste_vector
+
+
+def create_set_of_potential(data,URIS, W):
+    vector = list()
+    songs_uri=list()
+    for i in range(0, len(data)):
+        if SVCA.prediction(SVCA.calc_Wx(W, np.array(data[i]))) == 0:
+            vector.append(data[i])
+            songs_uri.append(URIS[i])
+    return vector,songs_uri
+
+
+def get_playlist_data(playlist_URI, Nlist,sp):
+    Matrix = []
+    Mlist = list()
+    for track in sp.playlist_items(playlist_URI)["items"]:
+        # URI
+        track_uri = track["track"]["uri"]
+        Mlist.append(sp.audio_features(track_uri)[0])
+        # Track name
+        track_name = track["track"]["name"]
+
+        # Main Artist
+        artist_uri = track["track"]["artists"][0]["uri"]
+        artist_info = sp.artist(artist_uri)
+
+        # Name, popularity, genre
+        artist_name = track["track"]["artists"][0]["name"]
+        artist_pop = artist_info["popularity"]
+        artist_genres = artist_info["genres"]
+
+        # Album
+        album = track["track"]["album"]["name"]
+
+        # Popularity of the track
+        track_pop = track["track"]["popularity"]
+
+    for i in range(0, len(Mlist)):
+        Nlist.append(list(Mlist[i].values())[:9])
+    return Nlist
+
+
+def get_user_data(data,sp):
+    Mlist = list()
+    Nlist = list()
+    URIS=list()
+    for i in range(0, len(data)):
+        # URI
+        track_uri = data[i]["track"]["uri"]
+        Mlist.append([sp.audio_features(track_uri)[0], track_uri])
+        URIS.append(track_uri)
+    for i in range(0, len(Mlist)):
+        Nlist.append(list(Mlist[i][0].values())[:9])
+    return Nlist,URIS
+
+
+
 
 
 def get_user_information(session):
